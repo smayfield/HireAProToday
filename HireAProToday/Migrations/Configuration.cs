172333136e58f510d5b5ff2c7b6b1ef9650a4cac
Scott Mayfield <mayfield@magicworld.com>
2015-08-09 10:44:17 -0400

    Enabled automatic data model migrations.

--- HireAProToday/Migrations/Configuration.cs
+++ HireAProToday/Migrations/Configuration.cs
@@ -11,7 +11,7 @@ namespace HireAProToday.Migrations
 
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
             ContextKey = "HireAProToday.Models.ApplicationDbContext";
 
             var migrator = new DbMigrator(this);
